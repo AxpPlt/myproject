2d414a5629981f92f1bd493b67766c78ae3013b4
Alex <alexplatach@gmail.com>
2022-01-10 01:20:09 +0200

    add code in view.js

--- assets/script/view.js
+++ assets/script/view.js
@@ -6,6 +6,7 @@ const btnAdd = document.querySelector(".btn-add");
 const btnMinus = document.querySelector(".btn-minus");
 const btnMultiple = document.querySelector(".btn-multiple");
 const btnDivide = document.querySelector(".btn-divide");
+// console.log('');
 
 const process = document.querySelector(".example");
 const result = document.querySelector(".outcome-result");
