dd1dceb5c0a16d5127974897c1b02e6da9f9f409
Alex <alexplatach@gmail.com>
2022-01-11 11:28:54 +0200

    Add script cal, only plus

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,9 +1,31 @@
 "use strict";
 let defaultResult = 0;
 let currentResult = defaultResult;
-
-let operand;
-
-function getUserNumberInput(inputFirst) {
+let mathOperator;
+function getUserNumberInputFirst() {
 	return parseInt(inputFirst.value);
 }
+function getUserNumberInputSecond() {
+	return parseInt(inputSecond.value);
+}
+function outPutUserResult(resultBeforeCalc, operator, userNumber, prevResultUser) {
+	const processUser = `${resultBeforeCalc} ${operator} ${userNumber}`;
+	outputResult(currentResult, processUser, prevResultUser);
+}
+function calculateUser(operatorType) {
+	const userNumberFirst = getUserNumberInputFirst();
+	const userNumberSecond = getUserNumberInputSecond();
+	const prevResult = currentResult;
+
+	switch (operatorType) {
+		case "ADD":
+			currentResult = userNumberFirst + userNumberSecond;
+			mathOperator = "+";
+			break;
+	}
+	outPutUserResult(userNumberFirst, mathOperator, userNumberSecond, prevResult);
+}
+function add() {
+	calculateUser("ADD");
+}
+btnAdd.addEventListener("click", add);
--- assets/script/view.js
+++ assets/script/view.js
@@ -6,13 +6,14 @@ const btnAdd = document.querySelector(".btn-add");
 const btnMinus = document.querySelector(".btn-minus");
 const btnMultiple = document.querySelector(".btn-multiple");
 const btnDivide = document.querySelector(".btn-divide");
-// console.log('');
+// console.log(btnAdd.textContent);
 
 const process = document.querySelector(".example");
 const result = document.querySelector(".outcome-result");
+const prevResultUserView = document.querySelector(".pretResult");
 
-function outputResult(currentResult, calcDescription) {
+function outputResult(currentResult, processUser, prevResultUser) {
 	result.textContent = currentResult;
-	process.textContent = calcDescription;
+	process.textContent = processUser;
+	prevResultUserView.textContent = prevResultUser;
 }
-outputResult(0, 0);
